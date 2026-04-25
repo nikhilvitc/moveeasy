6832ea22d73cf83d01dd13c4150afe2fe38698ba
Jiyanshu Dhaka <jiyanshudhaka2003@gmail.com>
2026-04-25 16:26:28 +0530

    Refactor ProtectedRoute usage in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 import Home from "./pages/Home";
+import MapPage from "./pages/MapPage";
 import Services from "./pages/Services";
 import Listings from "./pages/Listings";
 import Guarantee from "./pages/Guarantee";
@@ -15,34 +16,13 @@ export default function App() {
         <Routes>
           {/* Public routes */}
           <Route path="/" element={<Home />} />
+          <Route path="/map" element={<MapPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/onboarding" element={<Onboarding />} />
-
           {/* Protected routes */}
-          <Route
-            path="/services"
-            element={
-              <ProtectedRoute>
-                <Services />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/listings"
-            element={
-              <ProtectedRoute>
-                <Listings />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/guarantee"
-            element={
-              <ProtectedRoute>
-                <Guarantee />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/services" element={<ProtectedRoute><Services /></ProtectedRoute>} />
+          <Route path="/listings" element={<ProtectedRoute><Listings /></ProtectedRoute>} />
+          <Route path="/guarantee" element={<ProtectedRoute><Guarantee /></ProtectedRoute>} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
